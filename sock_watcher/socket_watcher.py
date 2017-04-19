9b6b1d3ea5d67659d53a4d88b1ac30f495ced6c6
knighthunter09 <emailforshekhar@gmail.com>
2017-04-19 22:14:55 +0800

    Draft1

--- sock_watcher/socket_watcher.py
+++ sock_watcher/socket_watcher.py
@@ -5,12 +5,52 @@ import time
 import watchdog
 import watchdog.events as ev
 import watchdog.observers as ob
+import zmq
+import socket
 
-class FileHandlerCustom(ev.LoggingEventHandler):
-    def on_any_event(self, event):
-        print("I am a nerd")
+
+def createSocket(sock_pull):
+    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    server_name = "localhost"
+    server_address = (server_name, 8084)
+    print(sys.stderr, 'starting up on %s port %s' % server_address)
+    sock.bind(server_address)
+    sock.listen(1)
+    return sock
+'''
+    while True:
+        print(sys.stderr, 'waiting for a connection')
+        connection, client_address = sock.accept()
+        try:
+            print(sys.stderr, 'client connected:', client_address)
+            while True:
+                message = sock_pull.recv()
+                print("Message received " + message)
+                connection.sendall(bytearray("hello22", 'utf8'))
+        finally:
+            connection.close()
+'''
 
 if __name__ == "__main__":
+
+    context = zmq.Context()
+    sock_push = context.socket(zmq.PUSH)
+    # sock_push.bind(sys.argv[1])
+    sock_push.bind(r"tcp://*:8083")
+
+    sock_pull = context.socket(zmq.PULL)
+    # sock_pull.bind(sys.argv[1])
+    sock_pull.connect(r"tcp://localhost:8083")
+
+    sock = createSocket(sock_pull)
+
+    class FileHandlerCustom(ev.LoggingEventHandler):
+        def on_any_event(self, event):
+            print("I am a nerd -- " + str(event.event_type))
+            print(sock_push)
+            sock_push.send_string(str(event.event_type))
+
+
     logging.basicConfig(level=logging.INFO,
                         format='%(asctime)s - %(message)s',
                         datefmt='%Y-%m-%d %H:%M:%S')
@@ -22,7 +62,21 @@ if __name__ == "__main__":
     observer.start()
     try:
         while True:
-            time.sleep(1)
+            #time.sleep(1)
+            print(sys.stderr, 'waiting for a connection')
+            connection, client_address = sock.accept()
+            try:
+                while True:
+                    message = sock_pull.recv()
+                    print("Message received " + message)
+                    connection.sendall(bytearray("hello22", 'utf8'))
+            finally:
+                connection.close()
     except KeyboardInterrupt:
         observer.stop()
     observer.join()
+
+
+
+
+
