da56e5cce26ef42abc982f64b264a09be5b64501
knighthunter09 <emailforshekhar@gmail.com>
2017-04-20 08:08:00 +0800

    Draft1

---
+++ sock_watcher/socket_message_sender.py
@@ -0,0 +1,38 @@
+import sys
+import zmq
+import socket
+
+
+def createSocket(sock_pull):
+    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    server_name = "localhost"
+    server_address = (server_name, 8084)
+    print(sys.stderr, 'starting up on %s port %s' % server_address)
+    sock.bind(server_address)
+    sock.listen(1)
+    return sock
+
+if __name__ == "__main__":
+
+    context = zmq.Context()
+    sock_pull = context.socket(zmq.PULL)
+    sock_pull.connect(r"tcp://localhost:8083")
+
+    sock = createSocket(sock_pull)
+
+    while True:
+        print(sys.stderr, 'waiting for a connection')
+        connection, client_address = sock.accept()
+        try:
+            print(sys.stderr, 'client connected:', client_address)
+            while True:
+                message = sock_pull.recv()
+                print("Message received " + message)
+                connection.sendall(bytearray("hello22", 'utf8'))
+        finally:
+            connection.close()
+
+
+
+
+
